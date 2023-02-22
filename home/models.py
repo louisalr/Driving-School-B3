f88ae3bd70e141c2294912b40559c25b6d5176f6
yso8 <baptisteguillaume5@gmail.com>
2023-02-22 15:34:42 +0100

    feat : add models to admin

--- home/models.py
+++ home/models.py
@@ -6,7 +6,7 @@ class School(models.Model):
     address = models.CharField(max_length=100)
     phone = models.CharField(max_length=10)
     email = models.CharField(max_length=50)
-    
+
 
 class Customer(AbstractUser):
     isManager = models.BooleanField(default=False)
@@ -17,5 +17,7 @@ class Booking(models.Model):
     school = models.ForeignKey(School, on_delete=models.CASCADE)
     customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
     date = models.DateField()
+
+
 
 
