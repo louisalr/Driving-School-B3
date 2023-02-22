c7ff42b09052f36b3744bc0918b549866a4fceeb
yso8 <baptisteguillaume5@gmail.com>
2023-02-22 14:19:23 +0100

    feat : initial models

--- home/models.py
+++ home/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User, AbstractUser
 
-# Create your models here.
+class School(models.Model):
+    name = models.CharField(max_length=50)
+    address = models.CharField(max_length=100)
+    phone = models.CharField(max_length=10)
+    email = models.CharField(max_length=50)
+    
+
+class Customer(AbstractUser):
+    isManager = models.BooleanField(default=False)
+    school = models.ForeignKey(School, on_delete=models.CASCADE, null=True)
+
+
+class Booking(models.Model):
+    school = models.ForeignKey(School, on_delete=models.CASCADE)
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    date = models.DateField()
+
+
